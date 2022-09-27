8dee27fe38b098245fc9441c5c7e8a115e162626
xocignacio <xose_ignacio@hotmail.com>
2022-09-27 16:08:29 -0300

    ultimo repo

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 import express from 'express'
+import __dirname from './utils.js';
 
 const app = express();
 const PORT = process.env.PORT;
-app.listen(PORT, ()=>  console.log(`- Server running on port  => ${PORT} ✔ `))
+app.listen(PORT,()=>  console.log(`- Server running on port  => ${PORT} ✔ `))
+
+app.use(express.static(__dirname+'/public'))
 
 app.get('/', (req,res)=>{
     console.log("hiii")
